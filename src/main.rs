0900332ebe75d40c906f8585ae62dbb51c90b1fc
Dan Ambrogio <dan.ambrogio@gmail.com>
2020-01-24 14:44:39 -0500

    implements a basic die roller

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 #[macro_use]
 extern crate clap;
+extern crate rand;
+
 use clap::App;
+use rand::Rng;
 
 fn main() {
   let yaml = load_yaml!("../cli.yml");
-  let _matches = App::from_yaml(yaml).get_matches();
+  let matches = App::from_yaml(yaml).get_matches();
+
+  let die = matches.value_of("die").unwrap_or("20");
+  let mut rng = rand::thread_rng();
+  let num = rng.gen_range(1, die.parse::<i32>().unwrap());
+  println!("You rolled {}! [0 - {}]", num, die);
 }
