7f30e0000a5ff7c6b04a04ee9d774c7a09eea032
Dan Ambrogio <dan.ambrogio@gmail.com>
2020-01-24 14:52:14 -0500

    implements an arg that tells the program how many dice to roll

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,15 @@ fn main() {
   let yaml = load_yaml!("../cli.yml");
   let matches = App::from_yaml(yaml).get_matches();
 
-  let die = matches.value_of("die").unwrap_or("20");
+  let die = matches.value_of("die").unwrap_or("20").parse::<i32>().unwrap();
+  let num = matches.value_of("num").unwrap_or("1").parse::<i32>().unwrap();
+
+  for _ in 0..num {
+    println!("You rolled {}! [0 - {}]", roll(die), die);
+  }
+}
+
+fn roll(die: i32) -> i32 {
   let mut rng = rand::thread_rng();
-  let num = rng.gen_range(1, die.parse::<i32>().unwrap());
-  println!("You rolled {}! [0 - {}]", num, die);
+  rng.gen_range(1, die)
 }
